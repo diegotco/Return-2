9e97ee00b42b5d4cec269aa0845d1d398dc0e3a0
diegotco <diegotco@yahoo.com>
2021-02-10 12:41:32 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,13 @@
+class Main {
+    public static void main(String[] args) throws Exception {
+        
+        entero();
+        System.out.println(entero());
+    }
+    
+    public static int entero(){
+        
+       int numero = 13;
+       return numero;
+    }
+}
